26102ecf112d73087a2fdf9a41fdb3570be1908f
Patrick Teichmann <patrick.teichmann@limesurvey.org>
2022-03-31 11:18:51 +0200

    Dev: replaced bootstrap.widgets with yiistrap.widgets

--- application/views/admin/participants/modal_subviews/_showParticipantSurveys.php
+++ application/views/admin/participants/modal_subviews/_showParticipantSurveys.php
@@ -17,7 +17,7 @@
     <input type="hidden" name="Participant[participant_id]" value="<?php echo $model->participant_id; ?>" />
     <div class="container-fluid">
     <?php
-        $this->widget('bootstrap.widgets.TbGridView', array(
+        $this->widget('yiistrap.widgets.TbGridView', array(
             'id' => 'list_participant_surveys',
             'dataProvider' => $surveymodel->search(),
             'columns' => $surveymodel->columns,
